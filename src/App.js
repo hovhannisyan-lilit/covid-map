0d25d9f4101a6a6d22ee4c7ca46b2331127ab759
hovhannisyan-lilit <lilithovhannisyan.004@gamil.com>
2023-05-15 12:16:48 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { type CSSProperties, memo } from 'react'
+import PropTypes from 'prop-types'
+import { GoogleMap, BicyclingLayer } from '@react-google-maps/api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ExampleBicyclingPropTypes = {
+    styles: PropTypes.shape({
+        container: PropTypes.object.isRequired,
+    }).isRequired,
 }
 
-export default App;
+const center: google.maps.LatLngLiteral = {
+    lat: 0,
+    lng: -180,
+}
+
+function onClick(...args: any[]) {
+    console.log('onClick args: ', args)
+}
+
+function onBicyclingLayerLoad (bicyclingLayer: google.maps.BicyclingLayer) {
+    // Do something with bicyclingLayer
+    console.log('bicyclingLayer: ', bicyclingLayer)
+}
+
+const onMapLoad = (map: google.maps.Map) => {
+    console.log('map: ', map)
+}
+
+interface Props {
+    styles: {
+        container: CSSProperties | undefined
+    }
+}
+
+function ExampleBicycling({ styles }: Props) {
+    return (
+        <div className='map'>
+            <div className='map-container'>
+                <GoogleMap
+                    id='bicycling-example'
+                    mapContainerStyle={styles.container}
+                    zoom={2}
+                    center={center}
+                    onClick={onClick}
+                    onLoad={onMapLoad}
+                >
+                    <BicyclingLayer onLoad={onBicyclingLayerLoad} />
+                </GoogleMap>
+            </div>
+        </div>
+    )
+}
+
+ExampleBicycling.propTypes = ExampleBicyclingPropTypes
+
+export default memo(ExampleBicycling)
